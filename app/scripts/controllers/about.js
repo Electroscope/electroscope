b881de5557daaa4b53beebf4ec345e960d6057b7
Su Thaw <suthaw524@gmail.com>
2015-09-06 23:00:42 +0630

    Create Project Architecutre with Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name electionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the electionApp
+ */
+angular.module('electionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
