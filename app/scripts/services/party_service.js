cf89d6dcd1da44b0c7b53e02b1628d60bfb0d7a5
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-09-08 00:33:23 +0630

    Add partyService with Ajax Call to mps API

---
+++ app/scripts/services/party_service.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name electionApp.PartyService
+ * @description
+ * # PartyService
+ * Factory in the electionApp.
+ */
+angular.module('electionApp')
+  .factory('partyService', ["$http", "$q", "mpsApiConfig", function($http, $q, mpsApiConfig){    
+
+    var getAllParties = function (){
+      var deferred = $q.defer();
+      var url = mpsApiConfig.baseURL + "/party?token=" + mpsApiConfig.token;
+      $http.get(url).
+        success(function(response) {
+          deferred.resolve(response.data);
+        })
+        .error(function(response) {
+          console.log("Cannot Get Server Data", response);
+          deferred.reject();
+        });
+      return deferred.promise;
+    }
+
+    var getPartyById = function (id){
+      var deferred = $q.defer();
+      var url = mpsApiConfig.baseURL + "/party/detail/" + id + "?token=" + mpsApiConfig.token;
+
+      $http.get(url).
+        success(function(response) {
+          deferred.resolve(response.data);
+        })
+        .error(function(response) {
+          console.log("Cannot Get Server Data", response);
+          deferred.reject();
+        });
+      return deferred.promise;
+    }
+
+    return {
+      getAllParties: getAllParties,
+      getPartyById: getPartyById
+    }
+  }]);
