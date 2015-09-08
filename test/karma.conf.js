c0eca7908ed571042d79cee1b75108840d247a68
DragonVirus <limonster.li@gmail.com>
2015-09-08 21:29:19 +0630

    Add angular-material to bower
    - remove bootstrap-sass-official
    - add base template for angular-material in index.html

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,11 +22,11 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
