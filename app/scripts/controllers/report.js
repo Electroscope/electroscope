6c79571c161d0119a48600c9b57f5326dbd7c917
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-09-10 09:36:32 +0630

    Add Sorting Checkbox for Dummy Bar Chart

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -80,20 +80,20 @@ angular.module('electionApp')
 
           // Copy-on-write since tweens are evaluated after a delay.
           var x0 = x.domain(dummyData.sort(this.checked
-              ? function(a, b) { return b.frequency - a.frequency; }
-              : function(a, b) { return d3.ascending(a.letter, b.letter); })
-              .map(function(d) { return d.letter; }))
+              ? function(a, b) { return b.count - a.count; }
+              : function(a, b) { return d3.ascending(a.party, b.party); })
+              .map(function(d) { return d.party; }))
               .copy();
 
           svg.selectAll(".bar")
-              .sort(function(a, b) { return x0(a.letter) - x0(b.letter); });
+              .sort(function(a, b) { return x0(a.party) - x0(b.party); });
 
           var transition = svg.transition().duration(750),
               delay = function(d, i) { return i * 50; };
 
           transition.selectAll(".bar")
               .delay(delay)
-              .attr("x", function(d) { return x0(d.letter); });
+              .attr("x", function(d) { return x0(d.party); });
 
           transition.select(".x.axis")
               .call(xAxis)
