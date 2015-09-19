ff0d132c18210b09aa9f3b77300651f0c43e1736
Thuya Myo Nyunt <ty@kny.co>
2015-09-19 19:28:15 +0630

    Add gulp config, monment and some configuration fix

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var gutil = require("gulp-util");
+
+// External Library 
+var __BOWER = path.join(__dirname, "bower_components");
+var __JQUERY = path.join(__BOWER, "jquery/dist/jquery.min.js");
+var __MATERIAL_LITE = path.join(__BOWER, "material-design-lite/material.min.js");
+var __MOMENT = path.join(__BOWER, "moment/min/moment.min.js");
+// var __HIGHMAP = path.join(__BOWER, "highmaps-release/highmaps.js");
+
+// Internal Library
+var __LIBS = path.join(__dirname, "./app/assets/js/libs");
+// var __MARKER_CULSTERER = path.join(__LIBS, 'marker_clusterer.js');
+// var __MARKER_WITH_LABEL = path.join(__LIBS, 'marker_with_label_packed.js');
+// var __EVENT_TRACKER = path.join(__LIBS, 'eventTracker.js');
+
+var DEFAULT = [__JQUERY, __MATERIAL_LITE, __MOMENT];
+
+// JS Paths
+function jsPath(_path){
+  return path.join(__dirname + "./assets/js", _path);
+}
+
+function concatDefault(__DEFAULTS, assets, other){
+  var paths = [];
+  if(assets) paths = assets.map(jsPath);
+  if(other) paths = paths.concat(other);
+  return __DEFAULTS.concat(paths);
+}
+
+var PAGES = {
+  "candidates": DEFAULT 
+};
+
+// Watch task for gulp
+gulp.task('watch', function () {
+  gulp.watch("gulpfile.js", ["scripts"]);
+  gulp.watch("./assets/js/**/*.js", ['scripts']);
+});
+
+// Script gulp task for developement
+gulp.task('scripts', function () {
+  Object.keys(PAGES).forEach(function(pagename){
+    var dist = path.join(__dirname, "public/js", pagename);
+    var paths = PAGES[pagename];
+    gulp.src(paths)
+      .pipe(concat('app.min.js'))
+      .pipe(gulp.dest(dist))
+  });
+});
+
+gulp.task('scripts-production', function () {
+  Object.keys(PAGES).forEach(function(pagename){
+    var dist = path.join(__dirname, "public/js", pagename);
+    var paths = PAGES[pagename];
+    console.log(pagename, dist, paths);
+    gulp.src(paths)
+      .pipe(concat('app.min.js'))
+      .pipe(uglify({mangle:false}))
+      .pipe(gulp.dest(dist))
+  });
+});
+
+gulp.task('default', ['scripts', 'watch']);
+gulp.task('production', ['scripts-production']);
