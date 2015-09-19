0575deeeed5fec3abdb2b99ecbd86170bf819014
Thuya Myo Nyunt <ty@kny.co>
2015-09-19 19:04:20 +0630

    Add candidate page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Electriscope' });
+});
+
+
+router.get('/', function (req, res) {
+  res.render("candidates", { title: "electroscope"});
 });
 
 module.exports = router;
