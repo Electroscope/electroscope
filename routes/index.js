f99023a19dedadd74c841bdae9500290c6d84a20
Thuya Myo Nyunt <ty@kny.co>
2015-09-19 19:42:16 +0630

    add pagenames

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Electriscope' });
+  res.render('index', { title: 'Electriscope', page_name: "home" });
 });
 
-
 router.get('/candidates', function (req, res) {
-  res.render("candidates", { title: "electroscope"});
+  res.render("candidates", { title: "electroscope", page_name: "candidates"});
 });
 
 module.exports = router;
