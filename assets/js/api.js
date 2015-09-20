f194bde710623350a4a2a867984c1bd1f32269ec
Thuya Myo Nyunt <ty@kny.co>
2015-09-20 20:28:11 +0630

    add more route

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,21 +1,41 @@
 var electroscope = (function () {
   var base = "https:/immense-plateau-8391.herokuapp.com";
+  base = "http://localhost:5000";
 
-  function get(data, callback) {
+  function makeGetRequest(data, callback) {
     $.ajax(data)
       .error(function (xhr, errorText, err) {
-        callback(errorText);
+        console.log(xhr, errorText, err);
+        callback(err);
       }).success(function (data) {
         // all result are return
-        callback(null, data.data);
+        callback(null, data);
       });
   }
 
   var api = {
+    getCandidatesByLocation: function (query, callback) {
+      if (!callback && typeof query === "function")
+        callback = query;
+      makeGetRequest({
+        url: base + "/api/candidate-locations",
+        data: query
+      }, callback);
+    },
+    getCandidateBy: function (query, callback) {
+      if (!callback && typeof query === "function")
+        callback = query;
+      makeGetRequest({
+        url: base + "/api/candidate-locations",
+        data: query
+      }, callback);
+    },
     countByParty: function (query, callback) {
-      get({
+      if (!callback && typeof query === "function")
+        callback = query;
+      makeGetRequest({
         url: base + "/api/candidates/count-by-party",
-        query: query
+        data: query
       }, callback);
     }
   };
