3b521f975734480869a9575f0db4b94302258019
DragonVirus <limonster.li@gmail.com>
2015-09-24 17:01:41 +0630

    Fix side nav for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 /* Common JS */
 jQuery(function(){
 
+	$('.button-collapse').click(function(){
+		var sidenav = $('.side-nav');
+		sidenav.removeClass('menu-collapse');
+		sidenav.addClass('menu-expand');
+		sidenav.removeClass('.enable-collapse');
+		$('.menu-collapse .nav-menu-title').show();
+	});
+
 	$(".button-collapse").sideNav();
 
-	$('.side-nav').hover(function(){
+	$('.side-nav .enable-collapse').hover(function(){
 		$(this).toggleClass('menu-collapse');
 		$(this).toggleClass('menu-expand');
 	});
