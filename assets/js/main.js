578ff5c1889f6d8b1ed0ad2d1114b2c16279d5ac
DragonVirus <limonster.li@gmail.com>
2015-09-24 16:31:47 +0630

    change to collapse sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-$(".button-collapse").sideNav();
+/* Common JS */
+jQuery(function(){
+
+	$(".button-collapse").sideNav();
+
+	$('.side-nav').hover(function(){
+		$(this).toggleClass('menu-collapse');
+		$(this).toggleClass('menu-expand');
+	});
+
+});
