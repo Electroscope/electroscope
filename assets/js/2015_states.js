4efe2a6f2b23f8c66cec8e32c596baf011f3c2c4
dreamingblackcat <dreamingblackcat10@gmail.com>
2015-09-25 19:09:43 +0630

    State List Map View Working in State Page

--- assets/js/2015_states.js
+++ assets/js/2015_states.js
@@ -1,54 +1,63 @@
 (function(electroscope){
 
   var drawStateDetail = function(state){
-    // $.getJSON("http://localhost:3000/" + state + ".topojson", function(data){
-    //   var defaultColor = "steelblue";
-    //   var statePartyCountCache = null;
-    //   var options = {
-    //     element: '#state_map',
-    //     width: 400,
-    //     height: 600,
-    //     defaultColor: defaultColor,
-    //     metaKey: state,
-    //     regionNameField: "name",
-    //     regionCodeField: "ST_PCODE"
-    //   };
-    //   electroscope.drawD3Map(data, options)
-    //     .on('click', function(d){
-    //       console.log("Clicking");
-    //       d3.selectAll(".map_region")
-    //         .style("fill", defaultColor);
-    //       d3.select(this)
-    //         .style("fill", "red");
 
-    //       $('.candidate-list').html(loadingIndicator);
+    var preloader =   '<div class="preloader-wrapper big active">\
+                          <div class="spinner-layer spinner-blue-only">\
+                            <div class="circle-clipper left">\
+                              <div class="circle"></div>\
+                            </div><div class="gap-patch">\
+                              <div class="circle"></div>\
+                            </div><div class="circle-clipper right">\
+                              <div class="circle"></div>\
+                            </div>\
+                          </div>\
+                        </div>';
 
-    //       if(!statePartyCountCache){
-    //         console.log("Getting from Server");
-    //         $.getJSON("http://localhost:3000/api/candidates/count?group_by=state,party,parliament", function(response){
-    //           statePartyCountCache = response.data;
-    //           updateList(statePartyCountCache, d.properties.ST);
-    //         });
-    //       }else{
-    //         console.log("Getting from Cache");
-    //         updateList(statePartyCountCache, d.properties.ST);
-    //       }
+    $('#state-map').html(preloader);
+    $.getJSON("http://localhost:3000/seperate_topo_upper/" + state + ".topojson", function(data){
+      var defaultColor = "steelblue";
+      var statePartyCountCache = null;
+      var options = {
+        element: '#state_map',
+        width: 400,
+        height: 600,
+        defaultColor: defaultColor,
+        metaKey: state,
+        regionNameField: "name",
+        regionCodeField: "ST_PCODE"
+      };
+      electroscope.drawD3Map(data, options)
+        .on('click', function(d){
+          console.log("Clicking");
+          d3.selectAll(".map_region")
+            .style("fill", defaultColor);
+          d3.select(this)
+            .style("fill", "red");
+
+          $('.candidate-list').html(loadingIndicator);
+
+          if(!statePartyCountCache){
+            console.log("Getting from Server");
+            $.getJSON("http://localhost:3000/api/candidates/count?group_by=state,party,parliament", function(response){
+              statePartyCountCache = response.data;
+              updateList(statePartyCountCache, d.properties.ST);
+            });
+          }else{
+            console.log("Getting from Cache");
+            updateList(statePartyCountCache, d.properties.ST);
+          }
           
-    //   })
-    //   .on("mousemove", function(d,i) {
-    //     var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
-    //     tooltip
-    //         .classed("hidden", false)
-    //         .attr("style", "left:"+(mouse[0]+offsetL)+"px;top:"+(mouse[1]+offsetT)+"px")
-    //         .html(d.properties[regionNameField]);
-    //   })
-    //   .on("mouseout",  function(d,i) {
-    //     tooltip.classed("hidden", true)
-    //   });
+      })
+      .on("mousemove", function(d,i) {
+        var html = '<a class="waves-effect waves-light btn center">' + d.properties.constituency_name_en + "-" + d.properties.constituency_number +'</a>';
+        $('#state_map_label').html(html);
+      })
+      .on("mouseout",  function(d,i) {
+        $('#state_map_label').html('');
+      })
 
-    // });
-    $('#state-details').text(state);
-    console.log("Drawing", $('#state-details').text());
+    });
   }
 
   $(document).ready(function(){
