9a0756bdb4645dd1ea890ecd1b3b30bdbc12c5f7
Thuya Myo Nyunt <ty@kny.co>
2015-10-04 18:52:48 +0630

    Remove other unused page from route

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Electroscope', page_name: "home" });
 });
-
+/*
 router.get('/candidates', function (req, res) {
   res.render("candidates", { title: "electroscope", page_name: "candidates"});
 });
@@ -22,11 +22,11 @@ router.get('/party/details', function (req, res) {
 router.get('/maptest', function(req, res){
   res.render("maptest",{ title: "electroscope", page_name: "maptest"});
 });
-
+*/
 router.get('/2015/overall', function(req, res){
   res.render("2015/overall", { title: "electroscope", page_name: "2015_overall"});
 });
-
+/*
 router.get('/partials/agegroup', function(req, res){
   res.render("partials/agegroup",{ title: "electroscope", page_name: "2015_overall"});
 });
@@ -34,26 +34,28 @@ router.get('/partials/agegroup', function(req, res){
 router.get('/2010/parties',function(req,res){
   res.render("2010/parties",{title:"electroscope",page_name:"2010_parties"});
 });
+*/
 
 // 2015 Route group
 router.get('/2015/parties',function(req,res){
   res.render("2015/parties",{title:"electroscope",page_name:"2015_parties"});
 });
 
+/*
 router.get('/2015/parties/:id',function(req,res){
   res.render("2015/parties_details",{title:"electroscope",page_name:"2015_parties_details"});
 });
-
+*/
 
 router.get('/2015/states',function(req,res){
   res.render("2015/states",{title:"electroscope",page_name:"2015_states"});
 });
 
-
 router.get('/2010/results',function(req,res){
   res.render("2010/results",{title:"electroscope",page_name:"2010_results"});
 });
 
+/*
 router.get('/choropleth',function(req,res){
   res.render("choropleth",{title:"choropleth",page_name:"choropleth"});
 });
@@ -61,6 +63,6 @@ router.get('/choropleth',function(req,res){
 router.get('/2015/circle_test',function(req,res){
   res.render("2015/circle_test",{title:"circle_test",page_name:"circle_test"});
 });
-
+*/
 
 module.exports = router;
