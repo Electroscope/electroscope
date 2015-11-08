f912ec61e0f03c1d692586a2bbc89ae93b12365a
Thuya Myo Nyunt <ty@kny.co>
2015-11-09 05:05:01 +0630

    Verify to google

--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,60 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Electroscope, microscope for Myanmar Elections 2010, 2015', page_name: "home"});
+  res.render('index', {
+    title: 'Electroscope, microscope for Myanmar Elections 2010, 2015', 
+    page_name: "home",
+    baseUrl: req.baseUrl
+  });
 });
 
 router.get('/2015/overall', function(req, res){
-  res.render("2015/overall", {title: "Electroscope, 2015 Myanmar Election overview", page_name: "2015_overall"});
+  res.render("2015/overall", {
+    title: "Electroscope, 2015 Myanmar Election overview", 
+    page_name: "2015_overall",
+    baseUrl: req.baseUrl
+  });
 });
 
 // 2015 Route group
 router.get('/2015/parties',function(req,res){
-  res.render("2015/parties",{title:"Electroscope, 2015 Myanmar Election analytic over parties",page_name:"2015_parties"});
+  res.render("2015/parties",{
+    title:"Electroscope, 2015 Myanmar Election analytic over parties",
+    page_name:"2015_parties",
+    baseUrl: req.baseUrl
+  });
 });
 
 router.get('/2015/states',function(req,res){
-  res.render("2015/states",{title:"Electroscope, 2015 Myanmar Election analytic over regions",page_name:"2015_states"});
+  res.render("2015/states",{
+    title:"Electroscope, 2015 Myanmar Election analytic over regions",
+    page_name:"2015_states",
+    baseUrl: req.baseUrl
+  });
 });
 
 router.get('/2010/results',function(req,res){
-  res.render("2010/results",{title:"Electroscope, 2010 Myanmar Election result analytic",page_name:"2010_results"});
+  res.render("2010/results",{
+    title:"Electroscope, 2010 Myanmar Election result analytic",
+    page_name:"2010_results",
+    baseUrl: req.baseUrl
+  });
 });
 
 router.get('/about',function(req,res){
-  res.render("about",{title:"About electroscope",page_name:"aboutus"});
+  res.render("about",{
+    title:"About electroscope",
+    page_name:"aboutus",
+    baseUrl: req.baseUrl
+  });
 });
+
+
 /*
 router.get('/data-reference',function(req,res){
-  res.render("data-reference",{title:"electroscope",page_name:"data-reference"});
+  res.render("data-reference",{title:"electroscope",page_name:"data-reference"});,
+  baseUrl: req.baseUrl
 });
 */
-
 
 module.exports = router;
